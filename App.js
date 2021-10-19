d805c141ff499e0db88a12f2151586d389b471a6
Moufida-Elbsir <86603111+Moufida-Elbsir@users.noreply.github.com>
2021-10-19 23:58:26 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import First from './components/First';
+import Second from './components/Second';
+import Form from './compnents/Form';
+function App() {
+  return (
+    <div className="App">
+      <First />
+      <Second />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
